b2aa51ecf2f70b37143022788f29e5d041654fa4
Nikita Plotnikov 221RDB021 <67636595+Gallenmar@users.noreply.github.com>
2023-02-28 21:40:54 +0200

    first try

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -2,45 +2,28 @@
 
 import sys
 import threading
-import numpy
 
 
-    
 def compute_height(n, parents):
     # Write this function
     max_height = 0
     # Your code here
-    marked = [False] * n
-    height = [0] * n
-
-    for i in range(n):
-        if not marked[i]:
-            trace = [i]
-            marked[i]=True
-            height[i]=1
-            v= parents[i]
-            while v != -1:
-                if not marked[v]:
-                    marked[v]=True
-                    trace.append(v)
-                    for j in trace:
-                        height[j]+=1
-
-                    v= parents[v]
-                else:
-                    for j in trace:
-                        height[j]+=height[v]
-                    v = -1
-    # print(height)
-    max_height= numpy.max(height)
-
-
-    
     return max_height
 
 
 def main():
     # implement input form keyboard and from files
+    text = input()
+    if 'I' in text:
+        text = input()
+        list_1 = list(text.strip(" "))
+        print(list_1)
+        # mismatch = find_mismatch(text)
+        # if mismatch==0:
+        #     print("Success")
+        #     #print(mismatch)
+        # else:
+        #     print(mismatch)
     
     # let user input file name to use, don't allow file names with letter a
     # account for github input inprecision
@@ -48,18 +31,6 @@ def main():
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
     # call the function and output it's result
-
-    # text = input()
-    # if 'I' in text:
-    size = input()
-    text = input()
-    Ttree = text.split(" ")
-    Itree = []
-    for i in Ttree:
-        Itree.append(int(i))
-    size = len(Itree)
-    
-    print(compute_height(size,Itree))
 
 
 # In Python, the default limit on recursion depth is rather low,
